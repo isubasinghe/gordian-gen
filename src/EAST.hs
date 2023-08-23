86e5eb6073b4017415bc9e139bff0b2e21532ab3
isubasinghe <i.subasinghe@unsw.edu.au>
2023-08-23 15:45:57 +1000

    cachix and constructor names

--- src/EAST.hs
+++ src/EAST.hs
@@ -12,6 +12,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -20,10 +21,22 @@
 module EAST where
 
 import Data.Kind ()
-import Data.Proxy ( Proxy(..) )
+import Data.Proxy (Proxy (..))
+import GHC.Float (int2Float)
 import GHC.Generics
-    ( Generic(Rep), U1, K1, M1, type (:+:), type (:*:), D, C, datatypeName, Datatype)
-import GHC.Float(int2Float)
+  ( C,
+    Constructor,
+    D,
+    Datatype,
+    Generic (Rep),
+    K1,
+    M1,
+    U1,
+    conName,
+    datatypeName,
+    type (:*:),
+    type (:+:),
+  )
 
 data MyMaybe a = MyNothing | MyJust !a
   deriving (Generic)
@@ -39,6 +52,14 @@ data MyMaybe a = MyNothing | MyJust !a
 --               ('MetaSel
 --                  'Nothing 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
 --               (Rec0 Int)))
+--
+-- MyMaybe Int
+-- numberOfConstructors = 2
+-- maxSize = 32
+-- bitvecSize = 33
+-- smtName = "MyMaybe_of_Int"
+-- constructors = [Just "MyJust_of_Int", Just "Nothing"]
+-- constructorSizes = [Just 32, Just 1]
 
 -- >>> :kind! (Rep (MyMaybe (MyMaybe Int)))
 -- (Rep (MyMaybe (MyMaybe Int))) :: * -> *
@@ -52,21 +73,33 @@ data MyMaybe a = MyNothing | MyJust !a
 --                  'Nothing 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
 --               (Rec0 (MyMaybe Int))))
 
--- MyMaybe Int 
--- 33
--- "MyMaybeInt"
--- [Just 32, Nothing]
--- [Just "MyMaybeIntJust", Nothing]
+data MyEither a b = MyLeft !a | MyRight !b !b
+  deriving stock (Generic)
+  deriving anyclass (BitVecRepr)
 
--- MyMaybe (MyMaybe Int)
--- 34 
--- [Just 33, Nothing]
--- [Just "MyMaybeMyMaybeInt", Nothing]
-
+-- >>> :kind! (Rep (MyEither Int Int))
+-- (Rep (MyEither Int Int)) :: * -> *
+-- = D1
+--     ('MetaData "MyEither" "EAST" "main" 'False)
+--     (C1
+--        ('MetaCons "MyLeft" 'PrefixI 'False)
+--        (S1
+--           ('MetaSel
+--              'Nothing 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
+--           (Rec0 Int))
+--      :+: C1
+--            ('MetaCons "MyRight" 'PrefixI 'False)
+--            (S1
+--               ('MetaSel
+--                  'Nothing 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
+--               (Rec0 Int)
+--             :*: S1
+--                   ('MetaSel
+--                      'Nothing 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
+--                   (Rec0 Int)))
 
 data MyTriple = Blah | Blah2 | Blah3 | Blah4 | Blah5
   deriving stock (Generic)
-
 
 data MySingle = Single deriving (Generic)
 
@@ -97,11 +130,9 @@ data MyMediumOne = A1 !Int !Int !Int !Bool
 --                       'Nothing 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
 --                    (Rec0 Bool))))
 
-
-
 data MyManyCons a b = MMC !a | MMC1 !b
-  deriving stock Generic
-  deriving anyclass BitVecRepr
+  deriving stock (Generic)
+  deriving anyclass (BitVecRepr)
 
 -- >>> :kind! (Rep (MyManyCons Int Bool))
 -- (Rep (MyManyCons Int Bool)) :: * -> *
@@ -120,16 +151,6 @@ data MyManyCons a b = MMC !a | MMC1 !b
 --                  'Nothing 'NoSourceUnpackedness 'SourceStrict 'DecidedStrict)
 --               (Rec0 Bool)))
 --
--- MyManyCons Int Bool
--- 33
--- "MyManyConsIntBool"
--- [Just 32, Just 1]
--- [Just "MyManyConsInt", Just "MyManyConsBool"]
---
--- MyManyCons (MyMaybe Int) Int
--- 66
--- "MyManyConsMyMaybeIntInt"
--- [Just 33, Just 32]
 
 data MyBigOne = B1 !Int !Int !MyMediumOne | B2 !Int !Bool
   deriving stock (Generic)
@@ -159,12 +180,14 @@ class BitVecRepr a where
   smtName :: Proxy a -> String
   default smtName :: GBitVecRepr (Rep a) => Proxy a -> String
   smtName _ = gsmtName (Proxy :: Proxy (Rep a))
-  
+
   constructorSizes :: Proxy a -> [Maybe Int]
+  default constructorSizes :: GBitVecRepr (Rep a) => Proxy a -> [Maybe Int]
+  constructorSizes _ = gconstructorSizes (Proxy :: Proxy (Rep a))
+
   constructors :: Proxy a -> [Maybe String]
-
-
-
+  default constructors :: GBitVecRepr (Rep a) => Proxy a -> [Maybe String]
+  constructors _ = gconstructors (Proxy :: Proxy (Rep a))
 
 -- Define the GSize type class and its instances
 class GBitVecRepr (f :: k) where
@@ -172,6 +195,7 @@ class GBitVecRepr (f :: k) where
   gmaxSize :: Proxy f -> Int
   gbitvecSize :: Proxy f -> Int
   gsmtName :: Proxy f -> String
+
   -- List of constructor sizes
   gconstructorSizes :: Proxy f -> [Maybe Int]
   gconstructors :: Proxy f -> [Maybe String]
@@ -180,7 +204,7 @@ instance {-# OVERLAPPABLE #-} GBitVecRepr f => GBitVecRepr (M1 i c f) where
   gnumberOfConstructors _ = gnumberOfConstructors (Proxy :: Proxy f)
   gmaxSize _ = gmaxSize (Proxy :: Proxy f)
   gbitvecSize _ = 0
-  gsmtName _ = "n"
+  gsmtName _ = gsmtName (Proxy :: Proxy f)
   gconstructorSizes _ = gconstructorSizes (Proxy :: Proxy f)
   gconstructors _ = gconstructors (Proxy :: Proxy f)
 
@@ -188,44 +212,66 @@ instance (GBitVecRepr f, Datatype c) => GBitVecRepr (M1 D c f) where
   gnumberOfConstructors _ = gnumberOfConstructors (Proxy :: Proxy f)
   gmaxSize _ = gmaxSize (Proxy :: Proxy f)
   gbitvecSize _ = gmaxSize (Proxy :: Proxy f) + fromCons
-    where 
-      fromCons :: Int 
+    where
+      fromCons :: Int
       fromCons = if numCons /= 1 then (+ (numCons `mod` 2)) . floor $ logBase 2 (int2Float numCons) else 0
       numCons = gnumberOfConstructors (Proxy :: Proxy f)
-      
-  gsmtName _ = datatypeName (undefined :: M1 D c f x) ++ gsmtName (Proxy :: Proxy f)
-  gconstructorSizes _ = undefined 
-  gconstructors _ = undefined
 
-instance (GBitVecRepr f) => GBitVecRepr (M1 C c f) where
+  gsmtName _ = datatypeName (undefined :: M1 D c f x) ++ (if rest /= "" then "_of_" ++ rest else "")
+    where
+      rest = gsmtName (Proxy :: Proxy f)
+  gconstructorSizes _ = gconstructorSizes (Proxy :: Proxy f)
+  gconstructors _ = gconstructors (Proxy :: Proxy f)
+
+instance (GBitVecRepr f, Constructor c) => GBitVecRepr (M1 C c f) where
   gnumberOfConstructors _ = 1
   gmaxSize _ = gmaxSize (Proxy :: Proxy f)
-  gbitvecSize _ = 0
-  gsmtName _ = "f"
+  gbitvecSize _ = error "should not be reached"
+  gsmtName _ = gsmtName (Proxy :: Proxy f)
+  gconstructors _ = [Just (currName ++ rest)]
+    where
+      currName = conName (undefined :: M1 C c f x)
+      sname = gsmtName (Proxy :: Proxy f)
+      rest = if sname /= "" then "_of_" ++ sname else ""
+  gconstructorSizes _ = error "hit this"
 
 instance (GBitVecRepr a, GBitVecRepr b) => GBitVecRepr (a :*: b) where
   gnumberOfConstructors _ = 0
   gmaxSize _ = gmaxSize (Proxy :: Proxy a) + gmaxSize (Proxy :: Proxy b)
-  gbitvecSize _ = 0
-  gsmtName _ = "a"
+  gbitvecSize _ = error "should not be reached"
+  gsmtName _ = left ++ "_prod_" ++ right
+    where
+      left = gsmtName (Proxy :: Proxy a)
+      right = gsmtName (Proxy :: Proxy b)
+  gconstructors _ = undefined
+  gconstructorSizes _ = undefined
 
 instance (GBitVecRepr a, GBitVecRepr b) => GBitVecRepr (a :+: b) where
   gnumberOfConstructors _ = gnumberOfConstructors (Proxy :: Proxy a) + gnumberOfConstructors (Proxy :: Proxy b)
   gmaxSize _ = max (gmaxSize (Proxy :: Proxy a)) (gmaxSize (Proxy :: Proxy b))
-  gbitvecSize _ = 0
-  gsmtName _ = "c"
+  gbitvecSize _ = error "should not be reached"
+  gsmtName _ = left ++ (if left /= "" && (right /= "") then "_sum_" else "") ++ right
+    where
+      left = gsmtName (Proxy :: Proxy a)
+      right = gsmtName (Proxy :: Proxy b)
+  gconstructors _ = gconstructors (Proxy :: Proxy a) ++ gconstructors (Proxy :: Proxy b)
+  gconstructorSizes _ = undefined
 
 instance GBitVecRepr U1 where
   gnumberOfConstructors _ = 0
   gmaxSize _ = 0
-  gbitvecSize _ = 0
-  gsmtName _ = "y"
+  gbitvecSize _ = error "should not be reached"
+  gsmtName _ = ""
+  gconstructors _ = undefined
+  gconstructorSizes _ = undefined
 
 instance (BitVecRepr a) => GBitVecRepr (K1 i a) where
   gnumberOfConstructors _ = numberOfConstructors (Proxy :: Proxy a)
   gmaxSize _ = maxSize (Proxy :: Proxy a)
-  gbitvecSize _ = 0
-  gsmtName _ = "k"
+  gbitvecSize _ = error "should not be reached"
+  gsmtName _ = smtName (Proxy :: Proxy a)
+  gconstructorSizes _ = []
+  gconstructors _ = []
 
 instance (BitVecRepr a) => BitVecRepr (MyMaybe a)
 
@@ -233,13 +279,17 @@ instance BitVecRepr Int where
   numberOfConstructors _ = 0
   maxSize _ = 32
   bitvecSize _ = 32
-  smtName _ = ""
+  smtName _ = "Int"
+  constructors _ = []
+  constructorSizes _ = []
 
 instance BitVecRepr Bool where
   numberOfConstructors _ = 0
   maxSize _ = 1
   bitvecSize _ = 1
-  smtName _ = ""
+  smtName _ = "Bool"
+  constructors _ = [Just "bvtrue", Just "bvfalse"]
+  constructorSizes _ = [Just 1, Just 1]
 
 myMaybePre :: MyMaybe Int -> Bool
 myMaybePre m = case m of
